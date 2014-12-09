a5a7397d78f8badd51cec9e24bb3de1bf4bbfb57
Gyuri Horak <dyuri@horak.hu>
2014-12-09 17:29:08 +0100

    +pir +led karacsonyi modban

--- dht22_lcd/dht22_lcd.ino
+++ dht22_lcd/dht22_lcd.ino
@@ -7,7 +7,8 @@ const int ledPin = 11;   // Teensy has LED on 11, Teensy++ on 6
 const int redPin =  12;
 const int greenPin =  15;
 const int bluePin =  14;
-const int dhtPin = 8;
+const int buttonPin = 8;
+const int PIRPin = 9;
 
 const int lightPin = 0;
 const int tempPin = 1;
@@ -22,39 +23,50 @@ LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 // the setup() method runs once, when the sketch starts
 
 void setup() {
+  Wire.begin();
   // initialize the digital pin as an output.
   pinMode(ledPin, OUTPUT);
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
-  pinMode(19, INPUT);
-  Serial.begin(38400);
+  pinMode(DHTPIN, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(PIRPin, INPUT);
+  Serial.begin(9600);
   
   dht.begin();
   
   lcd.begin(16, 2);
-  lcd.backlight();
+  lcd.noBacklight();
   lcd.home();
   lcd.print("Hello!");  
 }
 
 int redI = 0;
 int greenI = 0;
-int blueI = 0;
+int blueI = 255;
+int lastButton = LOW;
 
 void loop() {
   int i;
   int l;
+  int light;
   float temp;
   float dht_temp;
   float dht_hum;
+  int pir;
+  int button;
+ 
+  button = digitalRead(buttonPin);
+  pir = digitalRead(PIRPin);
 
   dht_temp = dht.readTemperature();
   dht_hum = dht.readHumidity();  
  
   analogRead(lightPin);
   delay(10);
-  l = map(analogRead(lightPin), 400, 1000, 0, 255);
+  light = analogRead(lightPin);
+  l = map(light, 400, 1000, 0, 255);
   
   analogRead(tempPin);
   delay(10);
@@ -76,27 +88,60 @@ void loop() {
   Serial.println(dht_hum);  
   Serial.println("<");
  
-  lcd.home();
-  lcd.print("Temp: ");
-  lcd.print(dht_temp);
-  lcd.setCursor(0, 1);
-  lcd.print("Hum%: ");
-  lcd.print(dht_hum);
+  if (lastButton != button) {
+    lcd.clear();
+    lastButton = button;
+  }
  
-  if (temp > 25) {
-    analogWrite(bluePin, 128);  
+  lcd.home();
+  if (pir == HIGH) {
+    lcd.backlight();
   } else {
-    analogWrite(bluePin, 0);
+    lcd.noBacklight();
   }
-  
-  if (l > 127) {
-    analogWrite(redPin, l);
-    analogWrite(greenPin, 0);
+  if (button == HIGH) {
+    lcd.print("Temp: ");
+    lcd.print(dht_temp);
+    lcd.setCursor(0, 1);
+    lcd.print("Hum%: ");
+    lcd.print(dht_hum);
+    Wire.beginTransmission(9);
+    Wire.send(200);
+    Wire.endTransmission();
   } else {
-    analogWrite(greenPin, l);
-    analogWrite(redPin, 0);
+    lcd.print("Temp (a): ");
+    lcd.print(temp);
+    lcd.setCursor(0, 1);
+    lcd.print("Light: ");
+    lcd.print(light);
+    Wire.beginTransmission(9);
+    Wire.send(0);
+    Wire.endTransmission();
   }
-
+ 
+  if (blueI == 255 && redI < 255 && greenI == 0) {
+    redI++;
+  }
+  if (redI == 255 && blueI > 0) {
+    blueI--;
+  }
+  if (redI == 255 && blueI == 0 && greenI < 255) {
+    greenI++;
+  }
+  if (greenI == 255 && redI > 0) {
+    redI--;
+  }
+  if (greenI == 255 && redI == 0 && blueI < 255) {
+    blueI++;
+  }
+  if (blueI == 255 && greenI > 0) {
+    greenI--;
+  }
+ 
+  analogWrite(bluePin, blueI);  
+  analogWrite(redPin, redI);
+  analogWrite(greenPin, greenI);
+ 
   delay(500);
   
 }
